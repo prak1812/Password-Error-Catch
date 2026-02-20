3b32078689d29afb5aae280d4d0ac062b6815917
Prakhar Sethi <prakharsethi05@gmail.com>
2026-02-20 22:43:04 +0530

    Backend Route Deployment Change

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 
 app.use("/api/auth", authRoutes);
 
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth"
+  baseURL: "https://password-error-catch.onrender.com/"
 });
 
 export default API;
