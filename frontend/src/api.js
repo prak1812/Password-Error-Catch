5a0d2b5d4406f22dd8b811a67e5220d9a39040b5
Prakhar Sethi <prakharsethi05@gmail.com>
2026-04-13 17:58:56 +0530

    Routes fixed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-   baseURL: "https://password-error.onrender.com/api/auth"
+   baseURL: "https://password-error.onrender.com"
 });
 
 export default API;
+
