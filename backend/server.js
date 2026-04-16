65659d89fac9c75b3b24f8d02e0dde550d1bd86c
Prakhar Sethi <prakharsethi05@gmail.com>
2026-04-16 20:24:27 +0530

    updated server logs

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,6 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
-console.log("BREVO KEY:", process.env.BREVO_API_KEY);
-console.log("CLIENT URL:", process.env.CLIENT_URL);
-console.log("MONGO URI:", process.env.MONGO_URI);
-console.log("JWT SECRET:", process.env.JWT_SECRET);
 connectDB();
 
 const app = express();
